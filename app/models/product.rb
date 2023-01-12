24e247033d4ceaa00da6dba103a6f305549d1cfa
Théo Courdil <courdiltheo@gmail.com>
2023-01-12 10:24:42 +0100

    create model for Product and attach image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    # has_one_attached :image
-    # validates :name, :color, :description, :price, presence: true
+    has_one_attached :image
+    validates :name, :color, :description, :price, presence: true
 end
