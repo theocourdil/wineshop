5f02d5ce42b509786c2886e323ba6f353b615ab8
Théo Courdil <courdiltheo@gmail.com>
2023-01-12 10:24:12 +0100

    rework root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'connexion', to: 'pages#connexion'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'    
+    get 'connect' => 'devise/sessions#connect'
+  end
+  
+  root to: 'products#index', as: 'home'
   resources :products
 end
