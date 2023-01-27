e79f8f7c2bd1eb9f374527fcd972f2ae58c33fb3
Théo Courdil <courdiltheo@gmail.com>
2023-01-27 18:33:30 +0100

    Add credit card table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_142325) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_214001) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_142325) do
     t.string "content_type"
     t.text "metadata"
     t.string "service_name", null: false
-    t.bigint "byte_size", null: false
+    t.integer "byte_size", null: false
     t.string "checksum"
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
@@ -39,11 +39,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_142325) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "credit_cards", force: :cascade do |t|
+    t.string "digits"
+    t.integer "month"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "color"
     t.text "description"
-    t.float "price"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
