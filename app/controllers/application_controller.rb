4c288fc6d89b6ad2d0a16e8c82309967525cd54c
Théo Courdil <courdiltheo@gmail.com>
2023-02-02 14:12:49 +0100

    Add query for search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     add_flash_types :danger, :info, :warning, :success, :messages
+    before_action :set_query
+
+    def set_query
+        @query = Product.ransack(params[:q])
+    end
 end
